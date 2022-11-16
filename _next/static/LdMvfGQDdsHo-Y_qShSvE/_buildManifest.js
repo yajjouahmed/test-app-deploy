d6d48a7b495f9ebbb0a68dcb6f5bb3f7076d6244
yajjouahmed <36079810+yajjouahmed@users.noreply.github.com>
2022-11-16 14:37:43 +0000

    Deploying to gh-pages from @ yajjouahmed/test-app-deploy@c20cc925d0ef571d0d292d7a3f9a885cf62721fe 🚀

---
+++ _next/static/LdMvfGQDdsHo-Y_qShSvE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4ed244536d3aa82e.css","static/chunks/pages/index-be34f99b0961c236.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
